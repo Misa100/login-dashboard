97ca255484afeceb69f7122e6a6ff17af9b535b3
Misa Ravololonantenaina <55345000+Misa100@users.noreply.github.com>
2024-11-15 09:16:13 +0300

    feat : fix responsive mobile v2.0

--- src/app/dashboard/sidenav-wrapper/sidenav-wrapper.component.ts
+++ src/app/dashboard/sidenav-wrapper/sidenav-wrapper.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import {Router, NavigationEnd} from '@angular/router';
-import {filter, map} from 'rxjs/operators'
+import { Component, OnInit, HostListener } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
 declare var handleSignout: any;
 
 @Component({
@@ -12,29 +12,40 @@ export class SidenavWrapperComponent implements OnInit {
   isExpanded: boolean = false;
   currentRouteName = '';
   userProfile: any;
+  isMobile: boolean = window.innerWidth < 768;
 
-  constructor(private router: Router){
+  constructor(private router: Router) {
     this.router.events.pipe(
-      filter((event): event is NavigationEnd=> event instanceof NavigationEnd),
+      filter((event): event is NavigationEnd => event instanceof NavigationEnd),
       map((event) => {
         let urlSegments = event.urlAfterRedirects.split('/');
-        return urlSegments[urlSegments.length -1];
+        return urlSegments[urlSegments.length - 1];
       })
     ).subscribe(routeName => {
-      this.currentRouteName = routeName.charAt(0).toUpperCase() + routeName.slice(1)
-    })
+      this.currentRouteName = routeName.charAt(0).toUpperCase() + routeName.slice(1);
+    });
   }
-  
-    ngOnInit() {
-      this.userProfile = JSON.parse(sessionStorage.getItem("loggedInUser") || "");
-    }
-    handleSignOut() {
-      handleSignout();
-      sessionStorage.removeItem("loggedInUser");
-      this.router.navigate(["/login"]).then(() => {
-        window.location.reload();
-      });
-    }
+
+  ngOnInit() {
+    this.userProfile = JSON.parse(sessionStorage.getItem("loggedInUser") || "");
+    this.adjustSidenav();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.isMobile = window.innerWidth < 768;
+    this.adjustSidenav();
+  }
+
+  adjustSidenav() {
+    this.isExpanded = !this.isMobile;
+  }
+
+  handleSignOut() {
+    handleSignout();
+    sessionStorage.removeItem("loggedInUser");
+    this.router.navigate(["/login"]).then(() => {
+      window.location.reload();
+    });
+  }
 }
-
-
